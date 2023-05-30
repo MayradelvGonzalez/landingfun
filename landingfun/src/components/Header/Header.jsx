9fde87d362e82f7ef3dcef62fcc230b8a0ea0b7b
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-30 12:11:32 -0300

    agregPosicion

--- landingfun/src/components/Header/Header.jsx
+++ landingfun/src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './header.css';
 
 function Header() {
   return (
-    <div><h1>MVG</h1>
-    
+    <div className='navegacion'><h1>MVG</h1>
+    <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Info</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+</ul>
     </div>
   )
 }
