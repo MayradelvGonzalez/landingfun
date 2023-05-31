095788318a91df7726c29fbfb301674669edd065
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-30 23:31:47 -0300

    agregComponentes

--- landingfun/src/components/Header/Header.jsx
+++ landingfun/src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './header.css';
 
+
 function Header() {
   return (
     <div className='navegacion'>
       <div> <h1>MVG</h1></div>
+<div></div>
       <div>
         <ul class="nav justify-content-center">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+            
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="../Footer/Footer.jsx">Contact</a>
           </li>
         </ul></div>
     </div>
