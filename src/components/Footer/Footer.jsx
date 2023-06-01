b9ae30059569da6055a1bfc68031b7d4f396ab34
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-06-01 17:59:45 -0300

    commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+function Footer() {
+  return (
+    <div className='foot'><h3>Contacto</h3><p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+
+
+  )
+}
+
+export default Footer
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
 import './header.css';
-
+import  avatar from '../../assets/avatar.png';
+import Footer from '../Footer/Footer';
 
 function Header() {
   return (
     <div className='navegacion'>
       <div> <h1>MVG</h1></div>
-<div></div>
+<div className='avatar'><img src={ avatar } alt="avatar" /></div>
       <div>
         <ul class="nav justify-content-center">
           <li class="nav-item">
             <a class="nav-link" aria-current="page" href="#">Home</a>
-            
           </li>
           <li class="nav-item">
             <a class="nav-link" href="../Footer/Footer.jsx">Contact</a>
           </li>
         </ul></div>
+        <div className="Aplicacion">
+    
     </div>
+    </div>
+
   )
 }
 
